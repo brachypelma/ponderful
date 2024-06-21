c3c3324d6fe9bf87a3fcb59cf94193e3d589e61a
Eric Jonas <ericdjonas@gmail.com>
2024-06-20 20:37:06 -0400

    Continued setting up basic styles

--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import classes from './question.module.scss'
+import { PencilEdit01Icon } from 'hugeicons-react'
 
 const Question = () => {
   return (
     <article className={classes.question}>
-      <h2>Would it matter to you if you had no free will?</h2>
+      <h2 className={classes.questionText}>
+        Would it matter to you if you had no free will?
+      </h2>
+      <section className={classes.bottom}>
+        <button className={classes.editButton}>
+          <PencilEdit01Icon color="#fcf8ff" />
+        </button>
+      </section>
     </article>
   )
 }
